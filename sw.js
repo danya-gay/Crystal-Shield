f672ab510a30aadb9e5cae50558df89ca8e1fbd5
danya-gay <maksim.iva15@gmail.com>
2026-01-13 10:26:29 +0200

    Implement notification click handler
    Add notification click listener to open site

--- sw.js
+++ sw.js
@@ -5,3 +5,14 @@ self.addEventListener('install', (event) => {
 self.addEventListener('activate', (event) => {
     event.waitUntil(clients.claim());
 });
+
+// Слушатель для клика по уведомлению (открывает сайт)
+self.addEventListener('notificationclick', (event) => {
+    event.notification.close();
+    event.waitUntil(
+        clients.matchAll({ type: 'window' }).then((clientList) => {
+            if (clientList.length > 0) return clientList[0].focus();
+            return clients.openWindow('/');
+        })
+    );
+});
